6b115310db0480fe5e7b6b795556fc2a1a30a167
Ken <khuang.dev@gmail.com>
2019-12-03 14:09:39 -0800

    added footer links

--- themes/isf-theme/footer.php
+++ themes/isf-theme/footer.php
@@ -10,9 +10,14 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
+				<div class="site-info content__footer">
 					<h4>Sign up for our newsletter!</h4>
 					<h4>Get in touch</h4>
+					<span class=link__footer>
+						<a>Careers</a>
+						<a>In the News</a>
+						<a>Contact Us</a>
+					</span>
 					<h4>Additional Information</h4>
 				</div><!-- .site-info -->
 				<div class="copyright">© 2017 | All rights reserved.</div>
