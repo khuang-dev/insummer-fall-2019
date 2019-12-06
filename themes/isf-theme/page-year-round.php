7dc8f291e86eff0f9b9689b2b9f00d831f47cf72
Kristine Suarez <suarezkristine_001@live.ca>
2019-12-06 14:35:59 -0800

    Added basic loop content to page-year-round.php

--- themes/isf-theme/page-year-round.php
+++ themes/isf-theme/page-year-round.php
@@ -9,12 +9,43 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-<?php the_post_thumbnail(); ?>
-        <?php the_field( 'banner_title' ); ?>
-        <?php the_field( 'banner_date' ); ?>
+			<?php the_post_thumbnail(); ?>
+			<?php the_field( 'banner_title' ); ?>
+			<?php the_field( 'banner_date' ); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header><!-- .entry-header -->
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+
+					<?php if(get_field('year_events')): ?>
+
+						<?php while(has_sub_field('year_events')): ?>
+
+							<?php the_sub_field('event_year'); ?>
+							<?php the_sub_field('year_event_about'); ?>
+							
+
+						<?php endwhile; ?>
+
+						<?php endif; ?>
+					
+					
+					<?php
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
+			</article><!-- #post-## -->
+
+
+				<!-- <php get_template_part( 'template-parts/content', 'page' ); ?> -->
 
 			<?php endwhile; // End of the loop. ?>
 
