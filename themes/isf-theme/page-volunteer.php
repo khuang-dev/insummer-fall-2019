1592800c94c4bcfd5382b32e92e08ab16676fc1f
Ken <khuang.dev@gmail.com>
2019-12-06 09:12:50 -0800

    updated footer, added missing pages for menu

---
+++ themes/isf-theme/page-volunteer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+<?php the_post_thumbnail(); ?>
+        <?php the_field( 'banner_title' ); ?>
+        <?php the_field( 'banner_date' ); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
