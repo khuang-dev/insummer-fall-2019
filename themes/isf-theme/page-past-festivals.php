cc72a58bd3b483aaeafd900e1ba1c00f42551335
Kristine Suarez <suarezkristine_001@live.ca>
2019-12-06 22:05:30 -0800

    Added basic loop content for page-past-festivals.php and updated page-membership.php

--- themes/isf-theme/page-past-festivals.php
+++ themes/isf-theme/page-past-festivals.php
@@ -18,13 +18,59 @@ get_header(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 				</header><!-- .entry-header -->
 
 				<div class="entry-content">
 					<?php the_content(); ?>
 
-					
+					<?php if(get_field('past_festival_testimonials')): ?>
+
+						<?php while(has_sub_field('past_festival_testimonials')): ?>
+
+							<p><?php the_sub_field('past_testimonial'); ?></p>
+							<p><?php the_sub_field('author_of_testimony'); ?></p>
+							<p><?php the_sub_field('festival_testimony_source'); ?></p>
+
+
+						<?php endwhile; ?>
+
+						<?php endif; ?>
+
+						<?php
+							if( have_rows('awards') ):
+								while ( have_rows('awards') ) : the_row();
+						?>
+									<h1><?php the_sub_field('awards_type');?></h1>
+									<p><?php the_sub_field('about_award');?></p>
+
+						<?php
+								endwhile;
+
+							else :
+								// no rows found
+							endif;
+
+							?>
+
+						<?php
+						if( have_rows('award_list') ):
+
+							while ( have_rows('award_list') ) : the_row();
+							?>
+
+								<p><?php the_sub_field('column_one'); ?></p>
+								<p><?php the_sub_field('column_two'); ?></p>
+								<p><?php the_sub_field('column_three'); ?></p>
+
+						<?php
+
+							endwhile;
+
+						else :
+							// no rows found
+						endif;
+						?>
+
 					<?php
 						wp_link_pages( array(
 							'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
@@ -38,10 +84,9 @@ get_header(); ?>
 
 				<!-- <php get_template_part( 'template-parts/content', 'page' ); ?> -->
 
-			<?php endwhile; // End of the loop. ?>
+		<?php endwhile; // End of the loop. ?> 
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
