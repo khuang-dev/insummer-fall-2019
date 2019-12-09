252105ae402e042d15c234733652d937351e2d87
Ken <khuang.dev@gmail.com>
2019-12-09 14:57:51 -0800

    added loops for front page testimony, featured page

--- themes/isf-theme/front-page.php
+++ themes/isf-theme/front-page.php
@@ -46,17 +46,18 @@ get_header(); ?>
 </div>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-            <section class="wrapper__upcoming-events">
+            
+        <section class="wrapper__upcoming-events">
                 <h2>Upcoming Events</h2>
                 <?php 
 				$args = array( 'post_type' => 'isf_event', 'order' => 'DSC', 'posts_per_page' => 3);
    				$event_posts = get_posts( $args ); // returns an array of posts
-			?>
+			    ?>
 			<div class="">
 
 			<?php foreach ( $event_posts as $post ) : setup_postdata( $post ); ?>
    				<?php /* Content from your array of post results goes here */ ?>
-					   <div class="">
+					   <div class="wrapper_single-event">
                         <img src="<?php the_field('event_image'); ?>">
                         <?php $date = new DateTime(get_field('event_date')); ?>
                         <div>
@@ -74,9 +75,35 @@ get_header(); ?>
 			<?php endforeach; wp_reset_postdata(); ?>
 			</div>
             </section>
+
             <section class="wrapper__testimony">
+                <?php the_field('testimony_header'); ?>
+                <?php the_field('testimony_image'); ?>
+                <?php the_field('testimony_video'); ?>
+                <?php if ( have_rows('testimony')):?>
+                            <?php while ( have_rows('testimony')) : the_row(); ?>
+                                <?php the_sub_field('testimony_text');?>
+                                <?php the_sub_field('testimony_author');?>
+                            <?php endwhile; ?>
+                            <?php else : ?>
+                            <?php endif; ?>            
             </section>
-            <section class="wrapper__highlight-page">
+
+            <section class="wrapper__featured">
+            <?php if ( have_rows('featured_page')):?>
+                            <?php while ( have_rows('featured_page')) : the_row(); ?>
+                                <img src="<?php the_sub_field('featured_page_image');?>">
+                                <?php the_sub_field('featured_page_title');?>
+                                <?php if ( have_rows('featured_page_description')):?>
+                                    <?php while ( have_rows('featured_page_description')) : the_row(); ?>
+                                    <?php the_sub_field('line_one');?>
+                                    <?php the_sub_field('line_two');?>
+                                <?php endwhile; ?>
+                                <?php else : ?>
+                                <?php endif; ?> 
+                            <?php endwhile; ?>
+                            <?php else : ?>
+                            <?php endif; ?> 
             </section>
 
 
