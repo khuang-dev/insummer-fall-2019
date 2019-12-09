186f3d9b113ab09737b9f27e504a5a8c131436cf
Tchore85 <prokopko1@gmail.com>
2019-12-08 18:25:29 -0800

    add repeater for img and icons accessibility 1

--- themes/isf-theme/page-accessibility.php
+++ themes/isf-theme/page-accessibility.php
@@ -18,7 +18,85 @@ get_header(); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
-<?php get_footer(); ?>
+						<!-- THIS IS THE LOOP FOR VIDEO NR.1 -->
+									<?php
+
+						// check if the repeater field has rows of data
+						if( have_rows('accessibility', ) ):   ?>
+
+							<div class="accessibility-video-loop">						
+
+							<?php
+										// loop through the rows of data
+										while ( have_rows('accessibility', ) ) : the_row();
+
+										// display a sub field value
+										?>
+
+							<div class="accessibility-about-info">
+							<div class="accessibility-about-title"> <?php  the_sub_field('title_accessibility', );?> </div>
+								<img class="accessibility-about-img" src="<?php the_sub_field('image_accessibility'); ?>"/>
+								<div class="accessibility-about-info"> <?php  the_sub_field('contact_information');?> </div>
+							</div>
+
+											<?php endwhile;
+											?>
+
+										</div>
+
+											<?php
+									else :
+										// no rows found
+									endif;
+
+									?>
+
+									<!-- END OF THE LOOP VIDEO NR.1 -->
+
+
+
+
+
+			<!-- THIS IS THE LOOP FOR ICONS NR.1 -->
+			<?php
+
+					// check if the repeater field has rows of data
+					if( have_rows('about_accessibility', ) ):   ?>
+
+						<div class="accessibility-info-loop">						
+
+						<?php
+									// loop through the rows of data
+									while ( have_rows('about_accessibility', ) ) : the_row();
+
+									// display a sub field value
+									?>
+
+						<div class="accessibility-info-item">
+						<div class="accessibility-title"> <?php  the_sub_field('information_title', );?> </div>
+							<img class="accessibility-icon-svg" src="<?php the_sub_field('accessibility_image'); ?>"/>
+							<div class="accessibility-info-details"> <?php  the_sub_field('information_details');?> </div>
+						</div>
+
+										<?php endwhile;
+										?>
+
+									</div>
+
+										<?php
+								else :
+									// no rows found
+								endif;
+
+								?>
+
+								<!-- END OF THE LOOP ICONS NR.1 -->
+
+
+
+
+										</main><!-- #main -->
+									</div><!-- #primary -->
+
+								<?php get_footer(); ?>
